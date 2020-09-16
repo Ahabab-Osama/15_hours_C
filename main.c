6cfa40706a3bdde2b8e0ee1deb0e1c37083a1f94
Ahabab-Osama <58246042+Ahabab-Osama@users.noreply.github.com>
2020-09-16 07:21:11 +0600

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display()
+{
+    printf("I am the genius\n");
+}
+
+int main()
+{
+    printf("Start!\n");
+    display();
+    printf("End!\n");
+    return 0;
+}
